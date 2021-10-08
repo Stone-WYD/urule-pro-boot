3d87fe4b79dcf241bdf001cd1a64c3d5b77b573f
rdxxjs <william.jiang@bstek.com>
2021-10-08 23:07:32 +0800

    支持jdbc驱动版本指定

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,10 @@
 mvn clean package -Dmaven.test.skip=true
 
-docker build -t williamjiang2021/urule-console-pro4:v1.0.2 .
+docker build -t bsteker/urule-console-pro4 .
 
-#docker commit -a "bstek" -m "urule-console-pro4" -p 93950e47f7d1 williamjiang2021/urule-console-pro4:v1.0.2
-#docker push williamjiang2021/urule-console-pro4:v1.0.2
-#docker run --name urule-console-pro -e URULE_PROFILE="hsql" -p 8080:8081 williamjiang2021/urule-console-pro4:v1.0.2
-#docker run --name urule-console-pro -p 8080:8081 williamjiang2021/urule-console-pro4:v1.0.2
-#docker pull williamjiang2021/urule-console-pro4:v1.0.2
+#docker commit -a "bstek" -m "urule-console-pro4" -p 93950e47f7d1 bsteker/urule-console-pro4:v1.0.0
+#docker push bsteker/urule-console-pro4:v1.0.0
+#docker run --name urule-console-pro -e URULE_PROFILE="hsql" -p 8080:8081 bsteker/urule-console-pro4:v1.0.0
+#docker run --name urule-console-pro -e URULE_PROFILE="mysql" -e DB_VERSION=5 -p 8080:8081 bsteker/urule-console-pro4
+#docker run --name urule-console-pro -p 8080:8081 bsteker/urule-console-pro4:v1.0.0
+#docker pull bsteker/urule-console-pro4:v1.0.0
--- home/urule-init.sh
+++ home/urule-init.sh
@@ -1,9 +1,99 @@
 echo "URULE_PROFILE=$URULE_PROFILE"
-if [ ! $URULE_PROFILE ] ;then
+echo "DRIVER_VERSION=$DRIVER_VERSION"
+
+resetInitProp(){
+	echo "reset urule-init.properties"
 	rm /urule/urule.properties
 	rm /urule/classes/urule-init.properties
 	cp /urule/classes/urule-init-db.properties /urule/classes/urule-init.properties
-	java -Xbootclasspath/a:/urule/classes -jar /urule/urule-pro-boot.jar
+}
+
+CLASSPATH="/urule/classes"
+JARFILE="/urule/urule-pro-boot.jar"
+LIBFILE=""
+
+if [ ! $URULE_PROFILE ] 
+then
+	resetInitProp
+	java -Xbootclasspath/a:/urule/classes -jar $JARFILE
+elif [ $URULE_PROFILE = 'dm' ] 
+then
+	resetInitProp
+
+	if [ $DRIVER_VERSION = 7 ] 
+	then
+		LIBFILE=":/urule/lib/dm/7/DmJdbcDriver17.jar"
+	elif [ $DRIVER_VERSION = 8 ] 
+	then
+		LIBFILE=":/urule/lib/dm/8/dm-connector-java-8.1.1.126.jar"
+	fi
+	CLASSPATH=${CLASSPATH}${LIBFILE}
+	echo "CLASSPATH=${CLASSPATH}"
+	java -Xbootclasspath/a:$CLASSPATH -jar $JARFILE 
+elif [ $URULE_PROFILE = 'mysql' ] 
+then
+	resetInitProp
+
+	if [ $DRIVER_VERSION = 5 ] 
+	then
+		LIBFILE=":/urule/lib/mysql/5/mysql-connector-java-5.1.49.jar"
+	elif [ $DRIVER_VERSION = 8 ] 
+	then
+		LIBFILE=":/urule/lib/mysql/8/mysql-connector-java-8.0.26.jar"
+	fi
+	CLASSPATH=${CLASSPATH}${LIBFILE}
+	echo "CLASSPATH=${CLASSPATH}"
+	java -Xbootclasspath/a:$CLASSPATH -jar $JARFILE
+elif [ $URULE_PROFILE = 'sqlserver' ] 
+then
+	resetInitProp
+
+	if [ $DRIVER_VERSION = 6 ] 
+	then
+		LIBFILE=":/urule/lib/sqlserver/6/mssql-jdbc-6.4.0.jre8.jar"
+	elif [ $DRIVER_VERSION = 7 ] 
+	then
+		LIBFILE=":/urule/lib/sqlserver/7/mssql-jdbc-7.4.1.jre8.jar"
+	elif [ $DRIVER_VERSION = 8 ] 
+	then
+		LIBFILE=":/urule/lib/sqlserver/8/mssql-jdbc-8.4.1.jre8.jar"
+	elif [ $DRIVER_VERSION = 9 ] 
+	then
+		LIBFILE=":/urule/lib/sqlserver/9/mssql-jdbc-9.4.0.jre8.jar"
+	fi
+	CLASSPATH=${CLASSPATH}${LIBFILE}
+	echo "CLASSPATH=${CLASSPATH}"
+	java -Xbootclasspath/a:$CLASSPATH -jar $JARFILE
+elif [ $URULE_PROFILE = 'oracle' ] 
+then
+	resetInitProp
+
+	if [ $DRIVER_VERSION = 12 ] 
+	then
+		LIBFILE=":/urule/lib/oracle/12/ojdbc8-12.2.0.1.jar"
+	elif [ $DRIVER_VERSION = 18 ] 
+	then
+		LIBFILE=":/urule/lib/oracle/18/ojdbc8-18.15.0.0.jar"
+	elif [ $DRIVER_VERSION = 19 ] 
+	then
+		LIBFILE=":/urule/lib/oracle/19/ojdbc8-19.12.0.0.jar"
+	elif [ $DRIVER_VERSION = 21 ] 
+	then
+		LIBFILE=":/urule/lib/oracle/21/ojdbc8-21.3.0.0.jar"
+	fi
+	CLASSPATH=${CLASSPATH}${LIBFILE}
+	echo "CLASSPATH=${CLASSPATH}"
+	java -Xbootclasspath/a:$CLASSPATH -jar $JARFILE
+elif [ $URULE_PROFILE = 'db2' ] 
+then
+	resetInitProp
+	LIBFILE=":/urule/lib/db2/db2jcc.jar"
+	CLASSPATH=${CLASSPATH}${LIBFILE}
+	echo "CLASSPATH=${CLASSPATH}"
+	java -Xbootclasspath/a:$CLASSPATH -jar $JARFILE
+elif [ $URULE_PROFILE = 'hsql' ] 
+then
+	java -Xbootclasspath/a:$CLASSPATH -jar $JARFILE --spring.profiles.active=$URULE_PROFILE
 else 
-	java -Xbootclasspath/a:/urule/classes -jar /urule/urule-pro-boot.jar --spring.profiles.active=$URULE_PROFILE
+	java -Xbootclasspath/a:$CLASSPATH -jar $JARFILE --spring.profiles.active=hsql
 fi
